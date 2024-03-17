8042fac227d027ebe281538bc54d52d2544f9fa0
Brian Sheridan <brian.sheridan@gmail.com>
2024-03-16 17:18:24 -0700

    Got React to work with Aspire

--- TossAndFetchScoring.AppHost/Program.cs
+++ TossAndFetchScoring.AppHost/Program.cs
@@ -4,10 +4,8 @@ var cache = builder.AddRedis("cache");
 
 var apiService = builder.AddProject<Projects.TossAndFetchScoring_ApiService>("apiservice");
 
-builder.AddNpmApp("react", "../TossAndFetchScoring.Web")
-    .WithReference(apiService)
-    .WithEndpoint(containerPort: 3001, scheme: "http", env: "PORT")
-    .AsDockerfileInManifest();
-
-
+builder.AddNpmApp("react", "../TossAndFetchScoring.Web","dev")
+    .WithReference(apiService) 
+    .WithEndpoint(5173,"http","frontend","PORT");
+    
 builder.Build().Run();
